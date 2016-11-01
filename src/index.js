2d1715cfba08a4da32377268134108ffbe28f8a1
Hernan Garchtrom <hernang87@gmail.com>
2016-10-31 21:49:43 -0300

    Fixed batch size

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ const RandomPinGenerator = require('./RandomPinGenerator');
 var rpg = new RandomPinGenerator(4, 3);
 
 var pin = rpg.getRandomPin();
-console.log(rpg.getBatch(10000));
+console.log(rpg.getBatch(1000));
